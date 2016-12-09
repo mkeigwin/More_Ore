4c3110db8747e295a2d646e70497d77666734423
Synclair Wang <synclairwang11@yahoo.com>
2016-12-09 16:08:32 -0500

    smelting adds to inventory right away

--- template/scripts/game.js
+++ template/scripts/game.js
@@ -2,7 +2,9 @@ $(() => {
 
   console.log('game.js connected')
 
-  let bOre = 0
+  let amountPerClick = 1
+
+  let bOre = 100
   let iOre = 0
   let gOre = 0
   let pOre = 0
@@ -17,12 +19,12 @@ $(() => {
   let miner = 0
   let heroes = 0
 
+  let pickaxeUpgradePrice = 10
+
+  let pickaxeLevel = 1
+
   let isSmelting = false
-
-
-
-
-
+  let smeltTime = 1000
 
 
   $('.bronze-ore-subtract').click(() => {
@@ -45,20 +47,43 @@ $(() => {
 
   $('.smelt-button').click(() => {
     if (isSmelting === false) {
-      isSmelting = true
-      $('.furnace-image').attr('src', './assets/furnace_active.png');
-      let amountOfBronze = $('.furnace-bronze-amount').text()
-      bOre -= (amountOfBronze * 10)
-      $('.bOre-amount').text(bOre)
-      $('.furnace-bronze-amount').text(0)
-      let timer = (5000 * amountOfBronze)
+      if ($('.furnace-bronze-amount').text() > 0) {
 
-      setTimeout(() => {
-        bBar+= parseInt(amountOfBronze)
-        isSmelting = false
-        $('.furnace-image').attr('src', './assets/furnace_inactive.png');
-        $('.bBar-amount').text(bBar)
-      }, timer)
+        isSmelting = true
+
+        $('.furnace-image').attr('src', './assets/furnace_active.png');
+        let amountOfBronze = $('.furnace-bronze-amount').text()
+        console.log(amountOfBronze)
+        bOre -= (amountOfBronze * 10)
+        $('.bOre-amount').text(bOre)
+        $('.furnace-bronze-amount').text(0)
+        let timer = (2000 * amountOfBronze)
+        $('.smelt-button').text(`Amount left: ${amountOfBronze}`)
+
+        let countdownTimer = setInterval(() => {
+          amountOfBronze--
+          console.log(amountOfBronze)
+          bBar++
+          $('.smelt-button').text(`Amount left: ${amountOfBronze}`)
+          $('.bBar-amount').text(bBar)
+
+          if (amountOfBronze <= 0) {
+            console.log(amountOfBronze)
+            console.log('clearing interval')
+            clearInterval(countdownTimer)
+            $('.smelt-button').text('SMELT')
+            isSmelting = false
+            $('.furnace-image').attr('src', './assets/furnace_inactive.png');
+          }
+        }, smeltTime)
+      }
+
+      // setTimeout(() => {
+      //   clearInterval(countdownTimer)
+      //   $('.smelt-button').text('SMELT')
+      //   isSmelting = false
+      //   $('.furnace-image').attr('src', './assets/furnace_inactive.png');
+      // }, timer)
     }
 
   })
@@ -71,8 +96,20 @@ $(() => {
     $('.upgrade-pickaxe-modal').css('display', 'none');
   })
 
+  $('.upgrade-pickaxe').click(() => {
+    if (bBar >= pickaxeUpgradePrice) {
+      bBar -= pickaxeUpgradePrice
+      $('.bBar-amount').text(bBar)
+      amountPerClick *= 2
+      pickaxeLevel++
+      pickaxeUpgradePrice *= 2.1
+      $('.upgrade-pickaxe-price').text(pickaxeUpgradePrice)
+      $('.pickaxe-level').text(pickaxeLevel)
+    }
+  })
+
   $('.rock').click(() => {
-    bOre++;
+    bOre += amountPerClick;
     $('.bOre-amount').text(bOre)
   })
 
